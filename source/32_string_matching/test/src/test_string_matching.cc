592940b1620852d9e4187129a3fd70c7da88db20
Timo Nicolai <timonicolai@arcor.de>
2018-10-28 18:22:41 +0100

    Improve string matching test style

--- source/32_string_matching/test/src/test_string_matching.cc
+++ source/32_string_matching/test/src/test_string_matching.cc
@@ -1,3 +1,4 @@
+#include <functional>
 #include <tuple>
 #include <vector>
 
@@ -7,22 +8,39 @@ extern "C" {
 #include "string_matching.h"
 }
 
-namespace {
+using ::testing::TestWithParam;
+using ::testing::Values;
 
-static std::vector<
-    std::tuple<char const *, char const *, std::vector<size_t>>> test_inputs{
 
-    std::make_tuple("abcabaabcabac", "abcabaabcabac", std::vector<size_t>({0})),
-    std::make_tuple("abcabaabcabac", "abaa", std::vector<size_t>({3})),
-    std::make_tuple("abcabaabcabac", "abc", std::vector<size_t>({0, 6})),
-    std::make_tuple("abcabaabcabac", "aba", std::vector<size_t>({3, 9})),
-    std::make_tuple("aaaab", "aa", std::vector<size_t>({0, 1, 2})),
-    std::make_tuple("aaaababa", "ba", std::vector<size_t>({4, 6})),
-    std::make_tuple("abc", "ba", std::vector<size_t>({}))
+class StringMatchTest
+    : public TestWithParam<std::function<std::size_t(char const*, char const*)>>
+{
+using test_input_type =
+    std::tuple<char const *, char const *, std::vector<size_t>>;
+
+protected:
+    std::vector<test_input_type> test_inputs {
+        std::make_tuple("abcabaabcabac", "abcabaabcabac",
+                        std::vector<std::size_t>({0})),
+        std::make_tuple("abcabaabcabac", "abaa",
+                        std::vector<std::size_t>({3})),
+        std::make_tuple("abcabaabcabac", "abc",
+                        std::vector<std::size_t>({0, 6})),
+        std::make_tuple("abcabaabcabac", "aba",
+                        std::vector<std::size_t>({3, 9})),
+        std::make_tuple("aaaab", "aa",
+                        std::vector<std::size_t>({0, 1, 2})),
+        std::make_tuple("aaaababa", "ba",
+                        std::vector<std::size_t>({4, 6})),
+        std::make_tuple("abc", "ba",
+                        std::vector<std::size_t>({}))
+    };
 };
 
-static void test_string_match(size_t (*match)(char const *, char const *))
+TEST_P(StringMatchTest, CanMatchPatterns)
 {
+    auto match = GetParam();
+
     for (auto const &test_input : test_inputs) {
         char const *text = std::get<0>(test_input);
         char const *comp = std::get<1>(test_input);
@@ -30,14 +48,10 @@ static void test_string_match(size_t (*match)(char const *, char const *))
         std::vector<size_t> expected = std::get<2>(test_input);
         std::vector<size_t> result;
 
-        size_t end_of_text = match(text, nullptr);
+        std::size_t end_of_text = match(text, nullptr);
 
-        size_t offs;
-        for (size_t i = 0u; i < expected.size(); ++i) {
-            offs = match(text, comp);
-
-            result.push_back(offs);
-        }
+        for (std::size_t i = 0u; i < expected.size(); ++i)
+            result.push_back(match(text, comp));
 
         EXPECT_EQ(end_of_text, match(text, comp))
             << "string matching algorithm returns end of text after all matches have been found";
@@ -47,20 +61,8 @@ static void test_string_match(size_t (*match)(char const *, char const *))
     }
 }
 
-TEST(StringMatchingTest, NaiveStringMatch) {
-    test_string_match(string_match_naive);
-}
-
-TEST(StringMatchingTest, RabinKarpMatch) {
-    test_string_match(string_match_rabin_karp);
-}
-
-TEST(StringMatchingTest, DFAMatch) {
-    test_string_match(string_match_dfa);
-}
-
-TEST(StringMatchingTest, KMPMatch) {
-    test_string_match(string_match_kmp);
-}
-
-}
+INSTANTIATE_TEST_CASE_P(StringMatchAlgorithms, StringMatchTest, Values(
+    string_match_naive,
+    string_match_rabin_karp,
+    string_match_dfa,
+    string_match_kmp));
